277cfd85cda40ccd6ff02e9b7f78d86bab432354
Nenetti <akinori1090@gmail.com>
2022-04-18 16:42:07 +0900

    WIP

--- docker/docker/runner/scripts/register.sh
+++ docker/docker/runner/scripts/register.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [[ -z "${REGISTRATION_TOKEN}" ]]; then
-  echo "Error: REGISTRATION_TOKEN environment variable not set."
+if [[ -z "${CI_SERVER_URL}" ]]; then
+  echo "Error: CI_SERVER_URL environment variable not set."
   exit 1
 fi
 
-if [[ -z "${CI_SERVER_URL}" ]]; then
-  echo "Error: CI_SERVER_URL environment variable not set."
+if [[ -z "${REGISTRATION_TOKEN}" ]]; then
+  echo "Error: REGISTRATION_TOKEN environment variable not set."
   exit 1
 fi
 
 gitlab-runner register \
   --non-interactive \
   --config "/etc/gitlab-runner/config.toml" \
-  --url "${URL}" \
+  --url "${CI_SERVER_URL}" \
   --registration-token "${REGISTRATION_TOKEN}" \
   --executor "docker" \
   --description "docker-runner" \
@@ -22,4 +22,5 @@ gitlab-runner register \
   --locked="false" \
   --access-level="not_protected" \
   --docker-image "ubuntu:20.04" \
-  --docker-gpus="all"
+  --docker-gpus="all" \
+  --docker-network-mode="host"
--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -25,14 +25,49 @@ start_docker_daemon
 #   Start GitLab Server & Runner
 #
 # ==================================================================================================
-docker-compose -f /root/docker/gitlab/docker-compose.yml stop t 0
-docker-compose -f /root/docker/runner/docker-compose.yml stop t 0
+SERVER_FILE=/root/docker/gitlab/docker-compose.yml
+RUNNER_FILE=/root/docker/runner/docker-compose.yml
 
-docker-compose -f /root/docker/gitlab/docker-compose.yml up --build
+docker-compose -f ${SERVER_FILE} stop t 0
+docker-compose -f ${RUNNER_FILE} stop t 0
 
-#docker-compose -f /root/docker/gitlab/docker-compose.yml up -d --build
-#docker-compose -f /root/docker/runner/docker-compose.yml up -d --build
-#docker-compose -f /root/docker/runner/docker-compose.yml up --build
+docker-compose -f ${SERVER_FILE} up -d --build
+
+while true; do
+  STATUS_CODE=$(curl "http://gitlab.example.com" -o /dev/null -w '%{http_code}\n' -s)
+  if [[ ${STATUS_CODE} = "302" ]]; then
+    break
+  fi
+  sleep 1
+done
+
+echo "Connected"
+docker-compose -f /root/docker/runner/docker-compose.yml up -d --build
+
+# ==================================================================================================
+#
+#   Backup (PIPE)
+#
+# ==================================================================================================
+#PIPE=/tmp/server.pipe
+#if [ -e ${PIPE} ]; then
+#  rm ${PIPE}
+#fi
+#mkfifo ${PIPE}
+#docker-compose -f ${SERVER_FILE} logs -f --since 0s | tee ${PIPE} >/dev/null &
+#PID=$!
+#
+#cat ${PIPE} | while read line; do
+#  if [[ ${line} =~ "Checking if we already upgraded: OK" ]]; then
+#    echo "a ${line}"
+#    break
+#  else
+#    echo "a ${line}"
+#  fi
+#done
+#
+#echo $PID
+#rm /tmp/server.pipe
 
 # ==================================================================================================
 #
